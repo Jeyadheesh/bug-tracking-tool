9a108fd56cf7d98c9f4ec1cdd65adf6565feca5c
Jeyadheesh <jeyadheesh.nagarajan@gmail.com>
2024-03-13 19:21:01 +0530

    removed default css styling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,27 +3,19 @@ import Image from "next/image";
 import useSWR from "swr";
 
 export default function Home() {
-  const { data, error, isLoading, isValidating } = useSWR(
-    "http://localhost:9000/getUsers",
-    fetcher
-  );
+  // const { data, error, isLoading, isValidating } = useSWR(
+  //   "http://localhost:9000/getUsers",
+  //   fetcher
+  // );
 
-  if (error) {
-    console.log(error);
-
-    return <div>error</div>;
-  }
-  if (isLoading) return <div>Loading...</div>;
+  // async function fetcher(url: string) {
+  //   const response = await fetch(url);
+  //   return await response.json();
+  // }
 
   return (
-    <ul>
+    <div>
       <h1>nice</h1>
-      <h1>{data.result}</h1>
-    </ul>
+    </div>
   );
-}
-
-async function fetcher(url: string) {
-  const response = await fetch(url);
-  return await response.json();
 }
